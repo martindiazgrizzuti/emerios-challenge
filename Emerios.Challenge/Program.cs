885360a15fe681f74e129fe4663945ee261536e4
Martin Díaz Grizzuti <martindiazgrizzuti@yahoo.com.ar>
2023-04-10 21:48:07 -0300

    Initial version

---
+++ Emerios.Challenge/Program.cs
@@ -0,0 +1,50 @@
+﻿using Emerios.Challenge;
+using System.Collections;
+using System.Diagnostics;
+
+if (args.Length == 1)
+{
+    try
+    {
+        /*string[] input =
+        new string[]
+        {
+            "B, B, B, B, D, L, F, D, L, 2",
+            "B, X, C, 7, D, J, K, D, 2, K",
+            "H, Y, 7, 3, E, D, 3, 2, K, 3",
+            "R, 7, O, Ñ, G, D, 2, K, D, 2",
+            "X, N, S, Ñ, E, 0, K, E, 0, D",
+            "A, 9, C, Ñ, D, E, F, D, E, F",
+            "A, X, D, Ñ, D, J, E, D, J, E"
+        };*/
+
+        // Read file
+        var input = Util.ReadFromFile(args[0]).Result;
+        
+        int columnCount = input.First().Length, rowCount = input.Length;
+        List<string> characters = new List<string>();
+        
+        var watch = Stopwatch.StartNew();
+        var res = Util.Process(input, rowCount, columnCount);
+        watch.Stop();
+
+        Console.WriteLine($"Finished process. Time elapsed: {watch.ElapsedMilliseconds} ms");
+        Console.WriteLine("Result:");
+        foreach (var value in res.Item1)
+        {
+            var text = String.Join(", ", ArrayList.Repeat(value, res.Item2).ToArray());
+            Console.WriteLine($"{text}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Ocurrió un error: {e.Message}");
+    }
+}
+else
+{
+    Console.WriteLine("Wrong args");
+}
+
+
+Console.ReadLine();
---
+++ Emerios.Challenge/Util.cs
@@ -0,0 +1,139 @@
+﻿namespace Emerios.Challenge;
+
+internal static class Util
+{
+    internal static async Task<string[][]> ReadFromFile(string path, string separator = ",")
+    {
+        if (!File.Exists(path))
+            throw new Exception("File doesn't exist in file provided");
+        var allLines = await File.ReadAllLinesAsync(path);
+        
+        if(allLines.Length == 0)
+            throw new Exception("File is empty");
+        
+        var allItems = allLines.Select(x => x.Split(separator).Select(ii => ii.Trim()));
+        var columnCount = allItems.First().Count();
+        if (allItems.Skip(1).Any(ii => ii.Count() != columnCount))
+            throw new Exception("Not all lines contain same column length");
+
+        return allItems.Select(ii => ii.ToArray()).ToArray();
+    }
+
+    internal static (List<string>, int) Process(string[][] data, int rowCount, int columnCount)
+    {
+        List<string> values = new List<string>();
+        int maxCount = 0;
+        int[][] horizontalValues = CreateZeroMatrix(rowCount, columnCount);
+        int[][] verticalValues = CreateZeroMatrix(rowCount, columnCount);
+        int[][] diagonalLeftValues = CreateZeroMatrix(rowCount, columnCount);
+        int[][] diagonalRightValues = CreateZeroMatrix(rowCount, columnCount);
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                // horizontal
+                if (j > 0)
+                {
+                    if (data[i][j] == data[i][j - 1])
+                        horizontalValues[i][j] = horizontalValues[i][j - 1] + 1;
+                    maxCount = CheckAdd(horizontalValues, data, i, j, maxCount, values);
+                }
+
+                // vertical
+                if (i > 0)
+                {
+                    if (data[i][j] == data[i - 1][j])
+                        verticalValues[i][j] = verticalValues[i - 1][j] + 1;
+                    maxCount = CheckAdd(verticalValues, data, i, j, maxCount, values);
+                }
+
+                // diagonal left
+                if (i > 0 && j < columnCount - 1)
+                {
+                    if (data[i][j] == data[i - 1][j + 1])
+                        diagonalLeftValues[i][j] = diagonalLeftValues[i - 1][j + 1] + 1;
+                    maxCount = CheckAdd(diagonalLeftValues, data, i, j, maxCount, values);
+                }
+
+                // diagonal right
+                if (i > 0 && j > 0)
+                {
+                    if (data[i][j] == data[i - 1][j - 1])
+                        diagonalRightValues[i][j] = diagonalRightValues[i - 1][j - 1] + 1;
+                    maxCount = CheckAdd(diagonalRightValues, data, i, j, maxCount, values);
+                }
+            }
+        }
+        return (values, maxCount + 1);
+    }
+
+    private static int CheckAdd(int[][] mat, string[][] data, int i, int j, int maxCount, List<string> values)
+    {
+        if (mat[i][j] >= maxCount)
+        {
+            if(mat[i][j] > maxCount)
+                values.Clear();
+            if(!values.Any(item => item == data[i][j]))
+                values.Add(data[i][j]);
+            return mat[i][j];
+        }
+        return maxCount;
+    }
+
+    private static int[][] CreateZeroMatrix(int rowCount, int columnCount)
+    {
+        int[][] matrix = new int[rowCount][];
+        for (int i = 0; i < rowCount; i++)
+            matrix[i] = new int[columnCount];
+        return matrix;
+    }
+
+    #region First Approach
+    /*
+    internal static int? Max(params int[] numbers)
+    {
+        if (numbers != null && numbers.Any())
+        {
+            int max = numbers.First();
+            foreach (var number in numbers.Skip(1))
+            {
+                max = number > max ? number : max;
+            }
+            return max;
+        }
+        return null;
+    }
+
+    internal static int CountRight(this string[][] input, int i, int j, int rowCount, int columnCount)
+    {
+        if (i >= 0 && i < rowCount && j >= 0 &&
+            j < columnCount - 1 && input[i][j + 1] == input[i][j])
+            return 1 + CountRight(input, i, j + 1, rowCount, columnCount);
+        return 1;
+    }
+
+    internal static int CountDown(this string[][] input, int i, int j, int rowCount, int columnCount)
+    {
+        if (j >= 0 && j < columnCount && i >= 0 &&
+            i < rowCount - 1 && input[i + 1][j] == input[i][j])
+            return 1 + CountDown(input, i + 1, j, rowCount, columnCount);
+        return 1;
+    }
+
+    internal static int CountDiagonalDownRight(this string[][] input, int i, int j, int rowCount, int columnCount)
+    {
+        if (j >= 0 && j < columnCount - 1 && i >= 0 && i < rowCount - 1 && input[i + 1][j + 1] == input[i][j])
+            return 1 + CountDiagonalDownRight(input, i + 1, j + 1, rowCount, columnCount);
+        return 1;
+    }
+
+    internal static int CountDiagonalDownLeft(this string[][] input, int i, int j, int rowCount, int columnCount)
+    {
+        if (j > 0 && j < columnCount && i >= 0 && i < rowCount - 1 && input[i + 1][j - 1] == input[i][j])
+            return 1 + CountDiagonalDownLeft(input, i + 1, j - 1, rowCount, columnCount);
+        return 1;
+    }
+    */
+    #endregion
+
+}
